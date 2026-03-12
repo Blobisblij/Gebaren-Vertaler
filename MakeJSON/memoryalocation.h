ee662cd8f98cd6c8548c6da31f4bd2b2de1c2229
Blobisblij <samthomsen@ziggo.nl>
2026-03-12 16:52:22 +0100

    memoalloc headerfile

--- MakeJSON/memoryalocation.h
+++ MakeJSON/memoryalocation.h
@@ -5,4 +5,10 @@
 #ifndef MAKEJSON_MEMORYALOCATION_H
 #define MAKEJSON_MEMORYALOCATION_H
 
+
+//allocates heap mem for an array
+int* createArrayInt(int size);
+
+float* createArrayfloat(int size);
+
 #endif //MAKEJSON_MEMORYALOCATION_H
