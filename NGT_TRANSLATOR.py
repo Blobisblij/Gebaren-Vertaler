2eb894094eb6a1887c0cd597c48533fce1fd63f0
Blobisblij <samthomsen@ziggo.nl>
2026-03-25 13:36:53 +0100

    test of je in python de c binary can execen

--- NGT_TRANSLATOR.py
+++ NGT_TRANSLATOR.py
@@ -1,1 +1,10 @@
+#!/venv/bin/python3
+import os
+
+
+exec(open("./MakeJSON/cmake-build-debug/MakeJSON"))
+pipe_path = "/tmp/Leapcam"
+with open(pipe_path, "r") as pipe:
+   data = pipe.read()
+   print(data)
 
