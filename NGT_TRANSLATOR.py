1d8ab895be71d25fdb0bb9efac7025903cb652a6
Blobisblij <samthomsen@ziggo.nl>
2026-04-07 09:46:47 +0200

    python dingetje

--- NGT_TRANSLATOR.py
+++ NGT_TRANSLATOR.py
@@ -1,20 +1,22 @@
 #!/venv/bin/python3
-import sys
+import platform
+
+import platform
 import os
 import time
 
-if os.name == "Windows":
+if platform.system() == "Windows":
    pipe_path = "\\\\.\\pipe\\Leapcam"
    time.sleep(2)
 
-if os.name == "Linux":
+if platform.system() == "Linux":
    pipe_path = "/tmp/Leapcam"
 
-if os.name == "Windows":
-   os.popen("cmake-build-debug/MakeJson.exe")
+if platform.system() == "Windows":
+   os.popen("MakeJSON/cmake-build-debug/MakeJson.exe")
 
-if os.name == "Linux":
-   os.popen("cmake-build-debug/MakeJson")
+if platform.system() == "Linux":
+   os.popen("MakeJSON/cmake-build-debug/MakeJSON")
 
 with open(pipe_path, "r") as pipe:
    data = pipe.read()
