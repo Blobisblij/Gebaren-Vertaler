bfd51607547b7912bd6f111aabd2d9b574ede927
Blobisblij <samthomsen@ziggo.nl>
2026-04-07 09:30:48 +0200

    open pipe en start programma

--- NGT_TRANSLATOR.py
+++ NGT_TRANSLATOR.py
@@ -1,7 +1,21 @@
 #!/venv/bin/python3
+import sys
+import os
+import time
 
+if os.name == "Windows":
+   pipe_path = "\\\\.\\pipe\\Leapcam"
+   time.sleep(2)
 
-pipe_path = "/tmp/Leapcam"
+if os.name == "Linux":
+   pipe_path = "/tmp/Leapcam"
+
+if os.name == "Windows":
+   os.popen("cmake-build-debug/MakeJson.exe")
+
+if os.name == "Linux":
+   os.popen("cmake-build-debug/MakeJson")
+
 with open(pipe_path, "r") as pipe:
    data = pipe.read()
 
