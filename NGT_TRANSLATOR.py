7b0a865bc9aa1b64ae0a3286140b6e4bc2888d6a
tvraap <tvraap18@gmail.com>
2026-04-09 11:29:48 +0200

    aerfada

--- NGT_TRANSLATOR.py
+++ NGT_TRANSLATOR.py
@@ -5,10 +5,10 @@ import platform
 import os
 import time
 
-from cv2.datasets import none
-from mpmath.libmp import to_int
-from sympy import true
-from urllib3.http2.probe import acquire_and_get
+#from cv2.datasets import none
+#from mpmath.libmp import to_int
+#from sympy import true
+#from urllib3.http2.probe import acquire_and_get
 
 if platform.system() == "Windows":
    pipe_path = "\\\\.\\pipe\\Leapcam"
@@ -17,15 +17,11 @@ if platform.system() == "Windows":
 if platform.system() == "Linux":
    pipe_path = "/tmp/Leapcam"
 
-if platform.system() == "Windows":
-   sp.Popen("./MakeJSON/cmake-build-debug/MakeJson.exe")
 
-#if platform.system() == "Linux":
-#   sp.Popen("MakeJSON/cmake-build-debug/MakeJSON")
 
 with open(pipe_path, "rb") as pipe:
    lijst =[]
-   lijstout = [none]
+   lijstout = None
    while lijstout != []:
       lijstout = []
       intdata = pipe.read(60)
@@ -37,6 +33,6 @@ with open(pipe_path, "rb") as pipe:
 
 
       print(lijstout)
-      intdata = none
+      intdata = None
 
 
