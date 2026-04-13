2b971a1f798fa33b42a4b8c939ec59fea9d2f448
tvraap <tvraap18@gmail.com>
2026-04-13 09:53:38 +0200

    een soort learn functie ding

--- NGT_TRANSLATOR.py
+++ NGT_TRANSLATOR.py
@@ -9,30 +9,32 @@ import time
 #from mpmath.libmp import to_int
 #from sympy import true
 #from urllib3.http2.probe import acquire_and_get
-
-if platform.system() == "Windows":
-   pipe_path = "\\\\.\\pipe\\Leapcam"
-
-
-if platform.system() == "Linux":
-   pipe_path = "/tmp/Leapcam"
+def Program_runnen(listgoingout):
+    if platform.system() == "Windows":
+       pipe_path = "\\\\.\\pipe\\Leapcam"
 
 
+    if platform.system() == "Linux":
+       pipe_path = "/tmp/Leapcam"
 
-with open(pipe_path, "rb") as pipe:
-   lijst =[]
-   lijstout = None
-   while lijstout != []:
-      lijstout = []
-      intdata = pipe.read(60)
-      for byte in intdata:
-         lijst.append(byte)
-         if len(lijst) == 4:
-            lijstout.append(int.from_bytes(lijst,byteorder ='little' , signed=True))
-            lijst = []
+    with open(pipe_path, "rb") as pipe:
+       lijst =[]
+       lijstout = None
+       totalinputdata = 0
+       maxinputdata = 100
+       while lijstout != []:
+          lijstout = []
+          intdata = pipe.read(60)
+          for byte in intdata:
+             lijst.append(byte)
+             if len(lijst) == 4:
+                lijstout.append(int.from_bytes(lijst,byteorder ='little' , signed=True))
+                lijst = []
+                print(lijstout)
+                ListGoingOut = tuple(lijstout)
+                totalinputdata = totalinputdata + 1
 
 
-      print(lijstout)
-      intdata = None
+
 
 
---
+++ Training_program.py
@@ -0,0 +1,70 @@
+#!/venv/bin/python3
+import platform
+import subprocess as sp
+import platform
+import os
+import time
+
+import NGT_TRANSLATOR
+# in de lijsten
+# plek 0 is laagste
+# plek 1 is hoogste
+#plek 3 is gemiddelde
+#plek vier is de hoeveelheid getallen in het huidige gemiddelde
+#plek 5 is het totaal
+#
+#
+#
+def main():
+
+    Input = input("which sign is it")
+    lijstout = []
+    NGT_TRANSLATOR.Program_runnen(lijstout)
+    List1 = [] * 5
+    List2 = [] * 5
+    List3 = [] * 5
+    List4 = [] * 5
+    List5 = [] * 5
+
+    for tuple in lijstout:
+        grouping(tuple[0], List1)
+        grouping(tuple[1], List1)
+        grouping(tuple[2], List1)
+        grouping(tuple[3], List1)
+        grouping(tuple[4], List1)
+
+def grouping (input, list):
+    if list == None:
+        krijgHoogste(input,None)
+        KrijgLaagste(input, None)
+        krijgGemiddelde(input, None)
+    else:
+        krijgHoogste(input,list)
+        KrijgLaagste(input, list)
+        krijgGemiddelde(input, list)
+
+
+def krijgGemiddelde(input, list):
+    if list[3] == None:
+        list[3] = input
+        list[4] = 1
+        list[5] = input
+    else:
+        list[4] = list[4] +1
+        list[5] = list[5] + input
+        list[3] = (list[5] / list[4])
+
+def KrijgLaagste(input, list):
+    if list[1] == None:
+        list[1] = input
+    else:
+        if input < list[1]:
+            list[1] = input
+
+def krijgHoogste(input, list):
+    if list[2] == None:
+        list[2] = input
+    else:
+        if input > list[2]:
+            list[2] = input
+
