454ab13e81fa086772a269bb3009c3593fcd8e90
tvraa <tvraap18@gmail.com>
2026-04-16 08:15:14 +0200

    ITS WORKINGGGGG hij kan gebaren herkennen

--- NGT_TRANSLATOR.py
+++ NGT_TRANSLATOR.py
@@ -10,10 +10,11 @@ from handmodel import HandNN, train, test
 from DatasetMaker import GestureDataset ,save_sample
 from torch.utils.data import DataLoader, random_split
 from DatasetMaker import Dataset
+
+import win32file
 #setting os spesific var's
 if platform.system() == "Windows":
     #pipe_path = "\\\\.\\pipe\\Leapcam"
-    import win32file
     pipe_path = r"\\.\pipe\Leapcam"
 
 if platform.system() == "Linux":
@@ -25,7 +26,7 @@ loss_fn = nn.CrossEntropyLoss()
 
 
 #collects data to train the model with
-if sys.argv[1] == "train":
+if sys.argv[1] == "collect":
     if sys.argv[2] != None:
         WordOrLetterToTrain = sys.argv[2]
 
@@ -40,7 +41,7 @@ if sys.argv[1] == "train":
 
         lijst = []
         totalinputdata = 0
-        maxinputdata = 120
+        maxinputdata = 500
 
         while totalinputdata < maxinputdata:
             lijstout = []
@@ -55,7 +56,9 @@ if sys.argv[1] == "train":
                 save_sample(lijstout, WordOrLetterToTrain)
                 totalinputdata += 1
 
-        win32file.CloseHandle(pipe)  # close after all samples collecte
+        win32file.CloseHandle(pipe)  # close after all samples collect
+        print(F"Collected {totalinputdata} samples for  {WordOrLetterToTrain}")
+if sys.argv[1] == "train":
         dataset = GestureDataset("gesture_dataset.csv")
         num_gestures = len(dataset.label_map)
         # laad het model met correcte params
@@ -75,35 +78,54 @@ if sys.argv[1] == "train":
         for epoch in range(epochs):
             train(train_loader,model,loss_fn,optimizer)
             test(test_loader,model,loss_fn)
-            torch.save(model, 'handmodel.pth')
-        else:
-            epochs = 15
-            for epoch in range(epochs):
-                train(train_loader,model,loss_fn,optimizer)
-                test(test_loader,model,loss_fn)
-                torch.save(model, 'handmodel.pth')
+
+        torch.save(model.state_dict(), 'handmodel.pth')
+
+        # else:
+        #     epochs = 15
+        #     for epoch in range(epochs):
+        #         train(train_loader,model,loss_fn,optimizer)
+        #         test(test_loader,model,loss_fn)
+        #         torch.save(model, 'handmodel.pth')
 
 
 
 
 #starts the datastream from the leap cam and sends the input to the moddel
 if (sys.argv[1] == "run"):
-    model = torch.load('handmodel.pth', weights_only=False)
-    with open(pipe_path, "rb") as pipe:
-        lijst =[]
-        lijstout = None
-        while lijstout != []:
-            lijstout = []
-            intdata = pipe.read(60)
-            for byte in intdata:
-                lijst.append(byte)
-                if len(lijst) == 4:
-                    lijstout.append(int.from_bytes(lijst,byteorder ='little' , signed=True))
-                    HandNN.eval()
-                    TensorLijstOut = ToTensor(lijstout)
-                    with torch.no_grad():
-                        prediction = HandNN(TensorLijstOut)
-                        if lastprediction != prediction:
-                            print(prediction)
-                            lastprediction = prediction
-                    lijst = []
+    dataset = GestureDataset("gesture_dataset.csv")
+    num_gestures = len(dataset.label_map)
+    model = HandNN(num_gestures).to(device)
+    model.load_state_dict(torch.load('handmodel.pth'))
+    model.eval()
+    index_to_label = {i: name for name, i in dataset.label_map.items()}
+    lastprediction = 0
+
+    # open pipe the windows way
+    pipe = win32file.CreateFile(
+        pipe_path,
+        win32file.GENERIC_READ,
+        0, None,
+        win32file.OPEN_EXISTING,
+        0, None
+    )
+    lijst = []
+    while True:
+        lijstout = []
+        hr, intdata = win32file.ReadFile(pipe, 60)
+        for byte in intdata:
+            lijst.append(byte)
+            if len(lijst) == 4:
+                lijstout.append(int.from_bytes(lijst,byteorder ='little' , signed=True))
+                lijst = []
+        if len(lijstout) == 15:
+            TensorLijstOut = torch.tensor(lijstout, dtype=torch.float32)
+
+            with torch.no_grad():
+                output = model(TensorLijstOut)
+                gesture_index =torch.argmax(output).item()
+                prediction = index_to_label[gesture_index]
+                if lastprediction != prediction:
+                    print(prediction)
+                    lastprediction = prediction
+    win32file.CloseHandle(pipe)
